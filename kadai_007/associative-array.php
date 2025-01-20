aae80bbcff2ac9524362f36f03268735eebb5202
saya0712 <yuuuhaaa1201@gmail.com>
2025-01-20 14:05:37 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>PHP基礎7章課題</title>
+</head>
+
+<body>
+  <p>
+    <?php
+    // 連想配列を作成
+    $product_data = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+    // $product_dataを画面に出力
+    print_r($product_data);
+    ?>
+  </p>
+</body>
+
+</html>
